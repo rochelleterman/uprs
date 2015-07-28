71bd3d36d4ea3d3da6a113b007873e0e9765a10f
Rochelle Terman <rterman@gmail.com>
2015-07-28 10:30:35 -0700

    adding matt's cown code

--- Cleaning/order.R
+++ Cleaning/order.R
@@ -2,12 +2,12 @@ library(plyr)
 
 rm(list=ls())
 setwd("~/Dropbox/berkeley/Dissertation/Data and Analyais/Git Repos/uprs/Data")
-data <- read.csv("all-binary.csv")
+data <- read.csv("all-cown-binary.csv")
 
 # all names
 names <- sort(names(data))
 # the core fields
-core <- c("id","to","From","text","year","notes","theme","institutions","action")
+core <- c("id","to","to-down","From","from-cown","text","year","notes","theme","institutions","action")
 # the non-core fields, or specific codes
 codes <- names[!names %in% core]
 # append them in order
