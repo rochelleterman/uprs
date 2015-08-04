eeaae56df5b676d7952610328ce1201bcda06849
Rochelle Terman <rterman@gmail.com>
2015-08-04 13:31:15 -0700

    clustering and text analysis

---
+++ Analysis/text-analysis/theme-clust.R
@@ -0,0 +1,137 @@
+rm(list=ls())
+setwd("~/Dropbox/berkeley/Dissertation/Data and Analyais/Git Repos/uprs/Analysis/text-analysis")
+
+library(plyr)
+library(RTextTools)
+library(qdap)
+library(stringr)
+library(mallet)
+library(corrgram)
+
+######################
+#### Prepare Data ####
+######################
+
+documents <- read.csv('~/Dropbox/berkeley/Dissertation/Data and Analyais/Git Repos/uprs/Data/all-cown-binary.csv', stringsAsFactors = F)
+names(documents)
+
+# make codes for country labels
+codes <- data.frame(cbind(documents$to,documents$to_COW,documents$year))
+codes <- unique(codes)
+names(codes) <- c("to","COW","year")
+codes <- arrange(codes,to,year)
+
+# reshape by report
+recs <- documents[,c(2,6,79:131)] #just take themes, country, year
+reports <- ddply(.data=recs, .variables=.(to,year), numcolwise(sum,na.rm = TRUE))
+reports <- reports[3:55]
+
+# just take themes in recs
+recs <- recs[,c(3:55)]
+
+# transpose recs to clustering themes by text
+recs.t <- data.frame(t(recs))
+
+#####################
+#### Correlations ###
+#####################
+
+# http://research.stowers-institute.org/mcm/efg/R/Visualization/cor-cluster/index.htm
+
+data = recs
+
+correlations <- as.data.frame(cor(data))
+x <- arrange(correlations,terrorism)
+corrgram(correlations)
+
+documents$text[documents$property==1]
+
+dissimilarity <- 1 - cor(data)
+distance <- as.dist(dissimilarity)
+round(distance, 4) 
+
+plot(hclust(distance), 
+  main="Dissimilarity = 1 - Correlation", xlab="")
+
+library(pvclust)
+cluster.bootstrap <- pvclust(correlations, nboot=1000, method.dist="abscor")
+plot(cluster.bootstrap)
+pvrect(cluster.bootstrap)
+
+####################
+#### K-Means ####
+####################
+
+data = recs.t
+
+set.seed(1234)
+n = 20
+clust <- kmeans(data,n, nstart= 25)
+centers <- as.data.frame(clust$centers) #make dataframe of cluster centers
+
+top5themes <- function(k){
+  theta.k <- centers[k,] # define theta-k, i.e.  row k of cluster centers dataframe
+  theta.notk <- colSums(centers[-(k),])/5 # define theta-not-k, i.e. rows not-k of cluster centers divided by 5 (number of clusters - 1)
+  diffk <- as.data.frame(theta.k - theta.notk) # define difference diffk
+  return(colnames(diffk[,order(diffk,decreasing=TRUE)][1:5])) # order decreasing, take top 10
+}
+keywords<- matrix(NA, nrow=5, ncol=n) # set up a matrix to contain data
+for (i in 1:n){
+  keywords[,i] <- top5themes(i)
+}
+keywords
+
+clusters <- as.data.frame(clust$cluster)
+clusters$theme <- rownames(clusters)
+colnames(clusters) <- c("cluster","theme")
+clusters$cluster <- as.factor(clusters$cluster)
+clusters <- arrange(clusters,cluster)
+
+# hclust
+distance.matrix <- dist(reports, method = "euclidean", diag = FALSE, upper = FALSE, p = 2)
+x<- hclust(distance.matrix, method = "complete", members = NULL)
+plot(x)
+
+###########
+### pca ###
+###########
+
+data = recs
+
+pr <- prcomp(data)
+plot(pr,type="l",main="Number of Components v. Variance Explained")
+
+library(ggplot2)
+scores = as.data.frame(pr$x)
+ggplot(data = scores, aes(x = PC1, y = PC2, label = rownames(scores))) +
+  geom_hline(yintercept = 0, colour = "gray65") +
+  geom_vline(xintercept = 0, colour = "gray65") +
+  geom_text(colour = "blue", alpha = 0.8, size = 4) +
+  ggtitle("PCA Plot")
+
+rotation <- as.data.frame(pr$rotation)
+ggplot(data = rotation, aes(x = PC1, y = PC2, label = rownames(rotation))) +
+  geom_hline(yintercept = 0, colour = "gray65") +
+  geom_vline(xintercept = 0, colour = "gray65") +
+  geom_text(colour = "red", alpha = 0.8, size = 4) +
+  ggtitle("PCA Rotation Plot")
+
+
+#######################
+### factor analysis ###
+#######################
+
+fit <- factanal(data, 6, rotation="varimax")
+print(fit, digits=2, cutoff=.3, sort=TRUE)
+# plot factor 1 by factor 2 
+load <- fit$loadings[,1:2] 
+plot(load,type="n") # set up plot 
+text(load,labels=names(data),cex=.7) # add variable names
+
+# Determine Number of Factors to Extract
+library(nFactors)
+ev <- eigen(cor(data)) # get eigenvalues
+ap <- parallel(subject=nrow(data),var=ncol(data),
+               rep=100,cent=.05)
+nS <- nScree(x=ev$values, aparallel=ap$eigen$qevpea)
+plotnScree(nS)
--- Analysis/text-analysis/topic-model.R
+++ Analysis/text-analysis/topic-model.R
@@ -6,6 +6,7 @@ library(RTextTools)
 library(qdap)
 library(stringr)
 library(mallet)
+library(cluster)
 
 # prepare stopwords list
 
@@ -20,10 +21,10 @@ documents$id <- rownames(documents)
 documents$text <- gsub(pattern="[[:punct:]]",replacement=" ",documents$text)
 
 # load data into mallet
-mallet.instances <- mallet.import(documents$id, documents$text, "Data/text-tools/stoplist_countries.csv", FALSE, token.regexp="[\\p{L}']+")
+mallet.instances <- mallet.import(documents$id, documents$text, "~/Dropbox/berkeley/Dissertation/Data and Analyais/Git Repos/uprs/Data/text-tools/stoplist_countries.csv", FALSE, token.regexp="[\\p{L}']+")
 
 ## Create a topic trainer object.
-topic.model <- MalletLDA(num.topics=20)
+topic.model <- MalletLDA(num.topics=100)
 
 ## Load our documents
 topic.model$loadDocuments(mallet.instances)
@@ -54,26 +55,30 @@ topic.model$maximize(10)
 doc.topics <- mallet.doc.topics(topic.model, smoothed=T, normalized=T)
 topic.words <- mallet.topic.words(topic.model, smoothed=T, normalized=T)
 
+# compare the text and topic proportions of one document
+documents$text[2]
+doc.topics[2,]
+
 ## What are the top words in topic X?
 ## Notice that R indexes from 1, so we have to look for row X+1.
-mallet.top.words(topic.model, topic.words[3,])
+mallet.top.words(topic.model, topic.words[4,])
 
 # take a quick look at the top words of each topic
 topic.labels <- mallet.topic.labels(topic.model, topic.words, num.top.words = 10)
 topic.labels
 
 ## Get a vector containing short names for the topics
-n.topics <- 20
+n.topics <- 100
 topics.labels <- rep("", n.topics)
 for (topic in 1:n.topics) 
-  topics.labels[topic] <- paste(mallet.top.words(topic.model, topic.words[topic,], num.top.words=10)$words, collapse=" ")
+  topics.labels[topic] <- paste(mallet.top.words(topic.model, topic.words[topic,], num.top.words=5)$words, collapse=" ")
 
 # have a look at keywords for each topic
 topics.labels
-write.csv(topics.labels,"20-topics-labels.csv")
+write.csv(topics.labels,"100-topics-labels.csv")
 
 ## Show the first few document titles with at least .5 of its content devoted to topic 4
-head(documents[ doc.topics[4,] > 0.5 , ],10)
+head(documents$text[ doc.topics[4,] > 0.5],3)
 
 ## Show title of the most representative text for topic 4
 documents[which.max(doc.topics[4,]),]$text
@@ -81,4 +86,22 @@ documents[which.max(doc.topics[4,]),]$text
 # plot clusters
 topics.labels
 plot(mallet.topic.hclust(doc.topics, topic.words, 0), labels=topics.labels)
+
+# method from here: http://www.r-bloggers.com/lda-on-ferguson-grand-jury-i/
+topic.docs <- t(doc.topics)
+topic.docs <- topic.docs / rowSums(topic.docs)
+topic_docs <- data.frame(topic.docs)
+names(topic_docs) <- documents$id
+sample <- sample(1:39329,1000)
+topic_docs <- topic_docs[,sample]
+
+topic_dist <- as.matrix(daisy(t(topic_docs), metric = "euclidean", stand = TRUE))
+topic_dist[ sweep(topic_dist, 1, (apply(topic_dist,1,min) + apply(topic_dist,1,sd) )) > 0 ] <- 0
+
+#Finally, we can use kmeans to identify groups of similar documents, and further get names for each cluster
+
+km <- kmeans(topic_dist, n.topics)
+alldocs <- vector("list", length = n.topics)
+for(i in 1:n.topics){
+  alldocs[[i]] <- names(km$cluster[km$cluster == i]) }
 
